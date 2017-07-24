1cbec700361b8f133f361a2367f143bcadcd589a
SaraPettinari <sara.pettinari@studenti.unicam.it>
2017-07-24 15:46:21 +0200

    Creato modello utente - per db

---
+++ js/models/utente.js
@@ -0,0 +1,16 @@
+// get an instance of mongoose and mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model and pass it using module.exports
+var User = mongoose.model('User', new Schema({
+    nome: { type: String, required: true },
+    cognome: { type: String, required: true },
+    matricola: { type: number, unique: true, required: true },
+    email: String,
+    username: { type: String, unique: true },
+    password: String,
+    admin: { type: Boolean, default: false }
+}));
+
+module.exports = User;
