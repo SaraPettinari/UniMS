de62b900ecd7a7007be48d4196b33d5f1031921f
SaraPettinari <sara.pettinari@studenti.unicam.it>
2017-07-25 11:43:59 +0200

    Aggiunti campi al mongooseSchema

--- js/server/models/utente.js
+++ js/server/models/utente.js
@@ -2,8 +2,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// set up a mongoose model and pass it using module.exports
-var User = mongoose.model('User', new Schema({
+var userSchema = new Schema({
     nome: { type: String, required: true },
     cognome: { type: String, required: true },
     matricola: { type: Number, unique: true, required: true },
@@ -11,6 +10,19 @@ var User = mongoose.model('User', new Schema({
     username: { type: String, unique: true },
     password: String,
     admin: { type: Boolean, default: false }
-}));
+});
 
+// custom method to add string to end of name
+// you can create more important methods like name validations or formatting
+// you can also do queries and find similar users 
+userSchema.methods.emailify = function () {
+    // add some stuff to the users name
+    //TODO controllo se è uno studente (@studenti...) o admin/professore
+    this.email = this.username + '@unims.it';
+
+    return this.email;
+};
+
+// set up a mongoose model and pass it using module.exports
+var User = mongoose.model('User', userSchema);
 module.exports = User;
