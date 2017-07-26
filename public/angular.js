68c6ca0909c3e8a95c59610e663da4582da2ec62
SaraPettinari <sara.pettinari@studenti.unicam.it>
2017-07-26 11:10:09 +0200

    Aggiunti primi moduli angularjs

---
+++ public/angular.js
@@ -0,0 +1,8 @@
+var app = angular.module("registrazione", []);
+app.module("nome", function($scope){
+    $scope.nome = "";
+});
+
+app.module("cognome", function($scope){
+    $scope.cognome = "";
+});
