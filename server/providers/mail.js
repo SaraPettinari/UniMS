3f9e70d0fd7f3674ff942dcf177de9fb7c59bd83
SaraPettinari <sara.pettinari@studenti.unicam.it>
2017-07-27 12:59:16 +0200

    Aggiunti template per l'inivio delle mail in automatico

--- server/providers/mail.js
+++ server/providers/mail.js
@@ -1,5 +1,5 @@
-var path           = require('path');
-var templatesDir   = path.join(__dirname, '../templates');
+var path = require('path');
+var templatesDir = path.join(__dirname, '../templates');
 var emailTemplates = require('email-templates');
 var config = require('../config/config');
 var utils = require("../config/utils");
@@ -7,110 +7,105 @@ var postmark = require('postmark')('9ac6881c-0f13-4d7c-8a4d-70ed0b450343');
 
 /** Mail Provider **/
 
-Mail = function(){};
+Mail = function () { };
 
 /** Send Welcome Mail **/
-Mail.prototype.welcome = function(json, lang, callback) {
-    
-    json.lang = lang;
-    
-    emailTemplates(templatesDir, function(err, template) {
+Mail.prototype.welcome = function (json, callback) {
+
+    emailTemplates(templatesDir, function (err, template) {
 
         if (err) callback(err, null);
         else {
-          
-            template(lang + '_welcome', json, function(err, html, text) {
+            template('../../views/mailTemplate/mailBenvenuto', json, function (err, html, text) {
                 if (err) callback(err, null);
-                else {                
+                else {
                     /** set mail **/
                     var mail = {
-                        From: 'UniMs <sarapetti10@gmail.com>',
+                        From: 'UniMs <servizi.unims@unims.it>',
                         To: json.email,
                         HtmlBody: html,
                         TextBody: text
                     };
                     console.log(mail);
-                
-                    /** send mail **/              
-                    postmark.send(mail, function(err, response) {
-                        if (err)    callback(err, null);
-                        else        callback(response, null);                    
+
+                    /** send mail **/
+                    postmark.send(mail, function (err, response) {
+                        if (err) callback(err, null);
+                        else callback(response, null);
                     });
-                    }
-            });          
-            
+                }
+            });
+
         }
     });
 };
 
 /** Send Activate Mail **/
-Mail.prototype.activate = function(json, lang, callback) {
-    
+Mail.prototype.activate = function (json, lang, callback) {
+
     json.lang = lang;
-    
-    emailTemplates(templatesDir, function(err, template) {
+
+    emailTemplates(templatesDir, function (err, template) {
 
         if (err) callback(err, null);
         else {
-          
-            template(lang + '_activate', json, function(err, html, text) {
-                
+
+            template('../../views/mailTemplate/mailAttivazione', json, function (err, html, text) {
+
                 if (err) callback(err, null);
-                else {             
+                else {
                     /** set mail **/
                     var mail = {
-                        From: 'UniMs <sarapetti10@gmail.com>',
+                        From: 'UniMs <servizi.unims@unims.it>',
                         To: json.email,
-                        Subject: subj,
                         HtmlBody: html,
                         TextBody: text
                     };
                     console.log(mail);
-                
-                    /** send mail **/              
-                    postmark.send(mail, function(err, response) {
-                        if (err)    callback(err, null);
-                        else        callback(null, response);                    
+
+                    /** send mail **/
+                    postmark.send(mail, function (err, response) {
+                        if (err) callback(err, null);
+                        else callback(null, response);
                     });
                 }
-            });          
-            
+            });
+
         }
     });
 };
 
 /** Resend Mail with new password **/
-Mail.prototype.resend = function(json, lang, pwd, callback) {
-    
+Mail.prototype.resend = function (json, lang, pwd, callback) {
+
     json.lang = lang;
     json.clean_password = pwd;
-    
-    emailTemplates(templatesDir, function(err, template) {
+
+    emailTemplates(templatesDir, function (err, template) {
 
         if (err) callback(err, null);
         else {
-          
-            template(lang + '_resend', json, function(err, html, text) {
+
+            template('../../views/mailTemplate/mailResetPassword', json, function (err, html, text) {
                 if (err) callback(err, null);
-                else {              
+                else {
                     /** set mail **/
                     var mail = {
-                        From: 'UniMs <sarapetti10@gmail.com>',
+                        From: 'UniMs <servizi.unims@unims.it>',
                         To: json.email,
-                        Subject: subj,
                         HtmlBody: html,
                         TextBody: text
                     };
                     console.log(mail);
-                
-                    /** send mail **/              
-                    postmark.send(mail, function(err, response) {
-                        if (err)    callback(err, null);
-                        else        callback(response, null);                    
+
+                    /** send mail **/
+                    postmark.send(mail, function (err, response) {
+                        if (err) callback(err, null);
+                        else callback(response, null);
                     });
-                    }
-            });          
-            
+                }
+            });
+
         }
     });
 };
