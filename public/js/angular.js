d887ee7ef44040a81f4426e52f19db9b37f898ac
Mara Albanesi <mara.albanesi@studenti.unicam.it>
2017-09-01 15:06:35 +0200

    modifica angular

--- public/js/angular.js
+++ public/js/angular.js
@@ -13,26 +13,6 @@ appH.controller('controllerHome', function ($scope) {
 
 var appA = angular.module("appAdmin", []);
 appA.controller('controllerAdmin', function ($scope) {
-    $scope.IsVisible1 = false;
-    $scope.activityC = 'Gestisci';
-    $scope.ShowHide1 = function () {
-        //If DIV is visible it will be hidden and vice versa.
-        $scope.IsVisible1 = $scope.IsVisible1 ? false : true;
-        if ($scope.IsVisible1 == false)
-            $scope.activityC = 'Gestisci';
-        else
-            $scope.activityC = 'Nascondi';
-    }
-    $scope.IsVisible2 = false;
-    $scope.activityD = 'Gestisci';
-    $scope.ShowHide2 = function () {
-        //If DIV is visible it will be hidden and vice versa.
-        $scope.IsVisible2 = $scope.IsVisible2 ? false : true;
-        if ($scope.IsVisible2 == false)
-            $scope.activityD = 'Gestisci';
-        else
-            $scope.activityD = 'Nascondi';
-    }
 });
 
 var appD = angular.module("appDocente", []);
